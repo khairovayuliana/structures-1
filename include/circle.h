40c6e45f51bdf059f296518cf00af789dc538335
khairovayuliana <y.khayrova@inbox.ru>
2025-06-03 09:14:01 +0300

    Update and rename point.h to circle.h

--- include/circle.h
+++ include/circle.h
@@ -5,6 +5,11 @@
 //     double y = 0.0;
 // };
 
+// declare Circle structure here
+struct Circle {
+  // your code here ("center" and "radius" (look for appropriate names in test.cpp))
+
+};
 double circleArea(Circle c);
 double circleLength(Circle c);
 bool isPointInside(Circle c, Point p); 
