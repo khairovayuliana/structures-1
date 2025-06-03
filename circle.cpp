ae493568aecb7f0bc21389be955a627625a0cb74
khairovayuliana <y.khayrova@inbox.ru>
2025-06-03 09:16:11 +0300

    Update circle.cpp

--- circle.cpp
+++ circle.cpp
@@ -1,9 +1,4 @@
-// #include <cmath>
+#include <cmath>
+#include "circle.h"
 
-#include "point.h"
-
-// double rectangleSquare(Point a, Point b) {
-//     double width = std::abs(b.x - a.x);
-//     double height = std::abs(b.y - a.y);
-//     return width * height;
-// }
+//  your code here (functions)
